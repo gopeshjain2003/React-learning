d212330181d4f68072fe669184d3ae41d38d904c
bhavya <gopeshjain2003@gmail.com>
2025-08-01 18:56:31 +0530

    Added 1 React Component

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,10 +1,12 @@
-
+import Chai from './chai' ;
 
 function App() {
 
   return (
     <>
       <h1>Chai aur React with Vite | Bhavya Jain</h1>
+      <Chai />    
+      <h3>Welcome To India</h3>
     </>
   )
 }

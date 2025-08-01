119dac433b8e289cabaa2041d4c52d0af875e45a
bhavya <gopeshjain2003@gmail.com>
2025-08-01 19:03:38 +0530

    Import chai.js

--- basic_react_01/src/App.js
+++ basic_react_01/src/App.js
@@ -1,8 +1,14 @@
 // Created using npx create-react-app 'application name'
+import Chai from './chai' ;     // First letter of React Component should be capital 
 
 function App() {
   return (
-    <h1>Chai aur React | Bhavya Jain</h1>  
+
+    <>
+      <h1>Chai aur React | Bhavya Jain</h1>  
+
+      <Chai />
+    </>
   );
 }
 

7282779549cb46f3f89b31a5e687ad2765af3682
bhavya <gopeshjain2003@gmail.com>
2025-08-02 11:50:04 +0530

    react_custom_element & creating react element + injecting the variable

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,9 +1,60 @@
-import { StrictMode } from 'react'            // React
-import { createRoot } from 'react-dom/client' // ReactDOM
+import React  from 'react'            // React
+import ReactDOM from 'react-dom/client' // ReactDOM
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// creating function and testing !
+function MyApp()
+{
+  return (
+    <div>
+      <h1>My Application from main.jsx !</h1>
+    </div>
+  )
+}
+
+
+// Invoking the same function of 'custom_react_02' ! Will NOT WORK !
+// Custom React Element (Behind the screne) .
+const ReactElement = {
+
+  type : 'a' , 
+  prop : {
+    href : 'https://google.com' ,
+    target : '_blank'
+  } ,
+  children : 'React Element '
+
+} ;
+
+// Will work outside of ReactElement
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Welcome to Google !</a>
+) ;
+
+
+// Injecting the final expression in ReactElement1 !
+const anotherUser = "Bhavya Mittal" ;
+
+
+// will work when is out React.StrictMode
+const ReactElement1 = React.createElement(
+  'a' ,
+  {href : 'https://google.com' , target : '_blank' } ,
+  'click me on reactElement' ,
+  anotherUser
+) ;
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  //   <App />
+  //   <MyApp/>
+    
+  // </React.StrictMode>,
+
+
+
+  // anotherElement
+  ReactElement1
 )

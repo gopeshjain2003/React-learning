173089f7a7dcf4f31f8648f2c4ecd54fc7135917
bhavya <gopeshjain2003@gmail.com>
2025-08-02 11:47:31 +0530

    custom Method & injecting final Expression

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,12 +1,27 @@
 import Chai from './chai' ;
 
+
+// Creating & testing the function 
+function Code()
+{
+  return (
+    <h3>Welcome to function Code() in App.jsx</h3>
+  )
+}
+
 function App() {
+
+  // It is final expression which can be injected in return section of the method signature ! 
+  const username = "Bhavya Jain" ;
 
   return (
     <>
       <h1>Chai aur React with Vite | Bhavya Jain</h1>
       <Chai />    
+      <Code/>
       <h3>Welcome To India</h3>
+      <br/>
+      Name is : <i>{username}</i>
     </>
   )
 }
